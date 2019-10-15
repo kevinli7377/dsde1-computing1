9c44eea1eb3cd80c5f905877691a10bd5a7ae145
Kevin Li <kl1019@ic.ac.uk>
2019-10-15 17:07:59 +0100

    Created FizzBuzz.py

---
+++ Week-3/FizzBuzz.py
@@ -0,0 +1,14 @@
+
+for n in range(1,100):
+     if n%3==0:
+          if n%5==0:
+              print('fizz-buzz')
+          elif n%5!=0:
+              print('fizz')
+     else:
+         if n%5==0:
+             print('buzz')
+         elif n%5!=0:
+             print('number')
+    
+
